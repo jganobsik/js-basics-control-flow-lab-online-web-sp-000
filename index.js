e00762c89ef0cb6055aa774164c6da8c5e584718
Joe Ganobsik <jganobsik@gmail.com>
2019-02-10 22:43:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if distance < 400
+  return 'This one is on me!'
+  
+  else if distance 
+  
+}
+
+function ternaryCheckCity(destination) {
+  
+}
+
+function switchOnCharmFromTip(tip) {
+  
+}
